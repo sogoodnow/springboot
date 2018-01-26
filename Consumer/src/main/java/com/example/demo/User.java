58afeda99da3ee7ffa070b7ed703321c3c73ab17
sogoodnow <sogoodnow@sina.com>
2018-01-26 18:43:37 +0800

    1

---
+++ Consumer/src/main/java/com/example/demo/User.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+public class User {
+	public User() {
+		super();
+
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	private String username;
+
+	private int age;
+
+	private Long id;
+
+}
