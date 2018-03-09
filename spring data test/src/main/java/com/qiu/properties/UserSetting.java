7a9a944b38e9a19428d48ef30fb0e8498e6b4741
sogoodnow <sogoodnow@sina.com>
2018-03-09 21:00:52 +0800

    44

---
+++ spring data test/src/main/java/com/qiu/properties/UserSetting.java
@@ -0,0 +1,33 @@
+package com.qiu.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties
+@PropertySource(value= "classpath:user.yml")
+public class UserSetting {
+	private String name;
+	private Integer age;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
